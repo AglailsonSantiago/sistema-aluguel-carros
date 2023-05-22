341a4e986b54d04d5cbd9ae5d163549af2f29217
unknown <aglailsonsantiago@gmail.com>
2023-05-22 13:37:06 -0300

    Integração spring boot e keycloak

--- backend-aluguel-carros/src/main/java/com/estudo/dev/backendaluguelcarros/repository/CarroRepository.java
+++ backend-aluguel-carros/src/main/java/com/estudo/dev/backendaluguelcarros/repository/CarroRepository.java
@@ -2,6 +2,8 @@ package com.estudo.dev.backendaluguelcarros.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.estudo.dev.backendaluguelcarros.model.Carro;
@@ -9,5 +11,10 @@ import com.estudo.dev.backendaluguelcarros.model.Carro;
 @CrossOrigin
 @RepositoryRestResource(path = "carros")
 public interface CarroRepository extends JpaRepository<Carro, Long>{
+
+    
+    // @PreAuthorize("hasRole('ROLE_ADMIN')")
+    // Carro save(Carro entity);
+
     
 }
---
+++ backend-aluguel-carros/src/main/java/com/estudo/dev/backendaluguelcarros/security/JwtAuthConverter.java
@@ -0,0 +1,60 @@
+package com.estudo.dev.backendaluguelcarros.security;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken>{
+
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+    private final JwtAuthConverterProperties properties;
+
+    public JwtAuthConverter(JwtAuthConverterProperties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public AbstractAuthenticationToken convert(Jwt value) {
+        Collection<GrantedAuthority> authorities = Stream.concat(
+                jwtGrantedAuthoritiesConverter.convert(value).stream(),
+                extractResourceRoles(value).stream()).collect(Collectors.toSet());
+        return new JwtAuthenticationToken(value, authorities, getPrincipalClaimName(value));
+    }
+
+    private String getPrincipalClaimName(Jwt jwt) {
+        String claimName = JwtClaimNames.SUB;
+        if (properties.getPrincipalAttribute() != null) {
+            claimName = properties.getPrincipalAttribute();
+        }
+        return jwt.getClaim(claimName);
+    }
+
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+        Map<String, Object> resource;
+        Collection<String> resourceRoles;
+        if (resourceAccess == null
+                || (resource = (Map<String, Object>) resourceAccess.get(properties.getResourceId())) == null
+                || (resourceRoles = (Collection<String>) resource.get("roles")) == null) {
+            return Set.of();
+        }
+        return resourceRoles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
+    }
+    
+}
